b35ecbfff43b35b46295321c21d351306c6f7a04
alajahroberts <ajr3339838@lpssonline.com>
2023-02-09 18:51:11 +0000

    refactor to create 100 circles using loops

--- projects/circularity/js/init.js
+++ projects/circularity/js/init.js
@@ -20,14 +20,25 @@ var init = function (window) {
         ////////////////////////////////////////////////////////////
         
         // TODO 1 : Declare and initialize our variables
-
+            var cirlce;
+            var circles = {};
 
         // TODO 2 : Create a function that draws a circle 
-        
+              function drawCircle();
+              cirlce = draw.randomCircleInAre(canvas, true, true, '#999', 2);
+    physikz.addRandomVelocity(circle, canvas);
+    view.addChild(circle);
+    circles.push(circle);
 
         // TODO 3 / 7 : Call the drawCircle() function 
-
-
+        drawCircle(0);
+        drawCircle(1);
+        drawCircle(2);
+        drawCircle(3);
+        drawCircle(4);
+        drawCircle(5);
+        
+        }
         ////////////////////////////////////////////////////////////
         ///////////////// PROGRAM LOGIC ////////////////////////////
         ////////////////////////////////////////////////////////////
@@ -39,13 +50,20 @@ var init = function (window) {
         */
         function update() {
             // TODO 4 : Update the circle's position //
-
-            
+            physikz.updatePosition(circle[0])
+            physikz.updatePosition(circle[1])
+            physikz.updatePosition(circle[2])
+            physikz.updatePosition(circle[3])
+            physikz.updatePosition(circle[4])
             // TODO 5 / 10 : Call game.checkCirclePosition() on your circles.
-           
+            game.checkCirclePosition(cirlce[0])
+            game.checkCirclePosition(circle[1])
+            game.checkCirclePosition(circle[2])
+            game.checkCirclePosition(circle[3])
+            game.checkCirclePosition(circle[4])
 
             // TODO 9 : Iterate over the array
-           
+        
             
         }
     
@@ -62,11 +80,16 @@ var init = function (window) {
             }
             
             // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-            
+        }  
+            if (circle. x > canvas.width) {
+                circle.x = 0;
+            }
 
 
             // YOUR TODO 6 CODE ENDS HERE //////////////////////////
-        }
+             if (circle.x < 1 ) {
+                circle.y = 0;
+             }
         
         /////////////////////////////////////////////////////////////
         // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
@@ -82,11 +105,12 @@ var init = function (window) {
         
         app.addUpdateable(window.opspark.game);
     }
-};
+
 
 // DON'T REMOVE THIS CODE //////////////////////////////////////////////////////
 if((typeof process !== 'undefined') &&
     (typeof process.versions.node !== 'undefined')) {
     // here, export any references you need for tests //
     module.exports = init;
-}
+
+    }
