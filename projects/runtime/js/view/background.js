eb7b3b8eba4490b89e62d9cb392a2bf6d6fe7481
alajahroberts <ajr3339838@lpssonline.com>
2023-05-12 17:49:54 +0000

    m

--- projects/runtime/js/view/background.js
+++ projects/runtime/js/view/background.js
@@ -78,8 +78,8 @@ var background = function (window) {
               }
             
             // TODO 4: Part 1 - Add a tree
-             tree = draw.bitmap("img/tree.png")
-             tree.x = 0
+             tree = draw.bitmap("img/tree.png");
+             tree.x = 0;
              tree. y = groundY + 20;
              background.addChild(tree);
             
